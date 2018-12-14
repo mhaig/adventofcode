387b6bc91eb78f268ffd659696d3ec76f81f6d38
Michael Haig <michael.haig@gmail.com>
2018-12-13 22:12:11 -0500

    Add part 2 solution to day 7

--- 2018/day07/day07.rb
+++ 2018/day07/day07.rb
@@ -81,3 +81,54 @@ while next_list.count > 0
 end
 
 p correct_order.join('')
+
+correct_order = []
+# Approach here is similar except steps go from next_list into worker queues and
+# then into correct_order.
+next_list = non_blocked_steps.sort
+
+workers = {0 => nil, 1 => nil, 2 => nil, 3 => nil, 4 => nil}
+
+timer = 0
+
+while true
+
+  # First tick the clock and move tasks as appropriate.
+  (0...workers.count).each do |i|
+    next unless workers[i]
+
+    workers[i][1] -= 1
+    if workers[i][1] == 0
+      correct_order << workers[i][0]
+      workers[i] = nil
+    end
+  end
+
+  # Build available task list.
+  possible_next_steps = get_possible_next_steps(correct_order)
+  next_list += possible_next_steps if possible_next_steps
+
+  next_list.sort!
+  # Remove any tasks in work!
+  next_list -= workers.values.map{ |v| v[0] if v }
+
+  # Try to assign the list to workers.
+  (0...workers.count).each do |i|
+
+    break unless next_list.count > 0
+    next if workers[i]
+
+    task = next_list.delete_at(0)
+    workers[i] = [task, task.ord - 'A'.ord + 1 + 60]
+
+  end
+
+  # p "#{timer}: #{workers}"
+
+  # See if we're done
+  break if workers.values.all? { |v| v == nil }
+
+  timer += 1
+end
+
+p timer
