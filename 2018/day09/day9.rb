4dcec77511b7740ba500093f5ce71aca06990958
Michael Haig <michael.haig@gmail.com>
2018-12-15 07:53:23 -0500

    Refactor to linked lists for day 9 part 2

--- 2018/day09/day9.rb
+++ 2018/day09/day9.rb
@@ -1,5 +1,44 @@
 #!/usr/bin/env ruby
 
+class Marble
+
+  attr_accessor :points, :next, :prev
+
+  def initialize(number, previous_marble=nil, next_marble=nil)
+    @points = number
+    @next = next_marble
+    @prev = previous_marble
+  end
+end
+
+def insert(current, new)
+
+  # Save current next.
+  tmp_next = current.next
+
+  current.next = new
+  new.prev = current
+  new.next = tmp_next
+  tmp_next.prev = new
+
+end
+
+def print(start)
+
+  start_save = start
+  a = start
+
+  p_array = []
+  loop do
+    p_array << a.points
+    a = a.next
+    break if a == start_save
+  end
+
+  p p_array
+
+end
+
 input = ARGF.read.split("\n")
 
 input.each do |i|
@@ -9,11 +48,13 @@ input.each do |i|
 
   scores = Hash.new(0)
   current_marble = 0
-  field = []
 
-  field << 0
+  head = Marble.new(0)
+  head.next = head
+  head.prev = head
+  position = head
+
   current_marble += 1
-  position = 0
 
   while current_marble <= marbles
 
@@ -26,9 +67,16 @@ input.each do |i|
 
         # The marble 7 marbles counter-clockwise from the current marble is
         # removed and the points given to the player.
-        position -= 7
-        position += field.count if position < 0
-        scores[p] += field.delete_at(position)
+        (1...7).each do
+          position = position.prev
+        end
+
+        # p position.points
+        scores[p] += position.points
+        tmp_prev = position.prev
+        tmp_next = position.next
+        tmp_next.prev = tmp_prev
+        tmp_prev.next = tmp_next
 
         # Position becomes the center for the next marble.
         current_marble += 1
@@ -39,33 +87,22 @@ input.each do |i|
 
       # Marble gets inserted at index of last marble plus 2 but wrapped around
       # if greater than current count.
-      position += 2
-      position -= field.count if position > field.count
-      field.insert(position, current_marble)
+      # Move ahead two marbles in list.
+      position = position.next
+      position = position.next
+
+      insert(position, Marble.new(current_marble))
+
+      # print(head)
+
       current_marble += 1
 
       break if current_marble > marbles
 
-      # spinner = Enumerator.new do |e|
-      #   loop do
-      #     e.yield '|'
-      #     e.yield '/'
-      #     e.yield '-'
-      #     e.yield '\\'
-      #   end
-      # end
-
-      percentage = ((current_marble/marbles) * 100)
-      if percentage % 5 == 0 and percentage > 0
-        # progress = "=" * percentage
-        # printf("\rCombined: [%-20s] %d%% %s", progress, percentage, spinner.next)
-        puts percentage
-      end
-
     end
 
   end
-  puts
+
   p scores.max_by { |k,v| v }
 
 end
