ac12560b5bfe7a5f4e0d3340fb2951bd322c1eb3
Michael Haig <michael.haig@gmail.com>
2019-12-30 21:49:37 -0500

    Add correct Day 14 Part 1 solution

--- 2019/day14.py
+++ 2019/day14.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # vim:set fileencoding=utf8: #
 
+class Chemical(object):
+
+    def __init__(self, name, amount):
+        self._name = name
+        self._amount = amount
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def amount(self):
+        return self._amount
+
+    def __str__(self):
+        return f'{self._amount} {self._name}'
+
 class Reaction(object):
     """Docstring for Reaction."""
 
@@ -15,13 +32,12 @@ class Reaction(object):
         self._inputs = []
         inputs, outputs = string.split('=>')
         for i in inputs.split(','):
-            self._inputs.append([int(i.split()[0]), i.split()[1]])
+            self._inputs.append(Chemical(i.split()[1], int(i.split()[0])))
 
-        self._output = outputs.split()
-        self._output[0] = int(self._output[0])
+        self._output = Chemical(outputs.split()[1], int(outputs.split()[0]))
 
-        self._is_fuel = self._output[1] == 'FUEL'
-        self._is_ore = self._inputs[0][1] == 'ORE'
+        self._is_fuel = self._output.name == 'FUEL'
+        self._is_ore = self._inputs[0].name == 'ORE'
 
     def is_fuel(self):
         return self._is_fuel
@@ -35,10 +51,10 @@ class Reaction(object):
         for i in self._inputs:
             if string:
                 string += ', '
-            string += f'{i[0]} {i[1]}'
+            string += str(i)
 
         string += ' => '
-        string += f'{self._output[0]} {self._output[1]}'
+        string += str(self._output)
 
         return string
 
@@ -52,33 +68,44 @@ class Reaction(object):
 
 def get_reaction(chemical, reactions):
     for r in reactions:
-        if r.output[1] == chemical:
+        if r.output.name == chemical:
             return r
 
-def get_required_ore(reaction, reactions):
+final_list = {}
+overages = {}
+def get_required_ore(amount, reaction, reactions):
 
-    # print(f'Get required ore for{reaction}')
+    print(f'Get required ore for {reaction}')
 
     if reaction.is_ore():
-        print(reaction.inputs[0][0])
-        return reaction.inputs[0][0]
+        print(f'Base element, adding {amount} to final list.')
+        final_list[reaction.output.name] += amount
+        return
     else:
-        required = 0
+        print(f'I need {amount} of {reaction.output.name}')
+        if overages.get(reaction.output.name, 0):
+            # Reduce amount by overage.
+            print(f'Applying existing overage of {overages[reaction.output.name]}')
+            amount -= overages[reaction.output.name]
+            overages[reaction.output.name] = 0
+        print(f'I need {amount} of {reaction.output.name}')
+        reaction_scale = math.ceil(amount / reaction.output.amount)
+        scaled_result = reaction_scale * reaction.output.amount
+        print(f'This reaction will produce {scaled_result} of ' +
+              f'{reaction.output.name}')
+        if scaled_result - amount:
+            print(f'overage of {scaled_result - amount}')
+            overages[reaction.output.name] = (
+                overages.get(reaction.output.name, 0) +
+                (scaled_result - amount))
         for i in reaction.inputs:
-            # ratio = (reaction.
-            a_reaction = get_reaction(i[1], reactions)
-            required += get_required_ore(a_reaction, reactions)
+            a_reaction = get_reaction(i.name, reactions)
+            get_required_ore(reaction_scale * i.amount, a_reaction, reactions)
 
-        return required
-
-def exp(x, y):
-
-    if y == 0:
-        return 1
-
-    return x * exp(x, y-1)
+        return
 
 import argparse
+import math
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file_name')
@@ -87,12 +114,17 @@ args = parser.parse_args()
 with open(args.file_name, 'r') as f:
     recipe = f.read()
 
+ores = {}
 reactions = []
 for line in recipe.split('\n'):
     if line:
         reactions.append(Reaction(line))
 
-print(reactions)
+        if reactions[-1].is_ore():
+            final_list[reactions[-1].output.name] = 0
+            ores[reactions[-1].output.name] = (reactions[-1].inputs[0].amount,
+                                               reactions[-1].output.amount)
+
 print('\n'.join([str(x) for x in reactions]))
 
 # Find the reaction that results in FUEL
@@ -102,6 +134,25 @@ for r in reactions:
         fuel = r
         break
 
-print(fuel)
+print(f'Fuel line: {fuel}')
 
-get_required_ore(fuel, reactions)
+# get_required_ore(fuel, reactions)
+r = 0
+for i in fuel.inputs:
+    a_reaction1 = get_reaction(i.name, reactions)
+    print(f'I need {i.amount} of {i.name}')
+    get_required_ore(i.amount, a_reaction1, reactions) 
+
+print(ores)
+print(final_list)
+
+# Use final_list to calculate required ore.
+total = 0
+for k,v in final_list.items():
+
+    # Get ratio for ore
+    ratio = ores[k]
+    print(f'Need {v} of {k} at {ratio[1]} {k} to {ratio[0]} ORE')
+    total += math.ceil(v / ratio[1]) * ratio[0]
+
+print(total)
