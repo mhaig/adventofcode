aac53c6aeb6607b0c2888419bf7929c3d04cc54b
Michael Haig <michael.haig@gmail.com>
2020-05-11 20:10:07 -0400

    Enhancements to Grid class
    Added ability to turn a grid into a string as well as to build a grid
    object from a string.

--- 2019/intcode/grid.py
+++ 2019/intcode/grid.py
@@ -16,6 +16,15 @@ class Grid(dict):
         if key[1] >= self._height:
             self._height = key[1] + 1
 
+    def __str__(self):
+        string = ''
+        for y in range(self.height):
+            for x in range(self.width):
+                string += str(self[x, y])
+            string += '\n'
+
+        return string
+
     @property
     def height(self):
         return self._height
@@ -26,3 +35,26 @@ class Grid(dict):
 
     def get_row(self, y):
         return [x for k,x in self.items() if k[1] == y]
+
+    def build(self, string):
+        for y, row in enumerate(string.split('\n')):
+            if not row:
+                continue
+
+            for x, c in enumerate(row):
+                if c:
+                    self[x,y] = c
+
+    def get_adjacent(self, x, y):
+
+        adjacent = []
+        if x:
+            adjacent.append(self[x-1, y])
+        if y:
+            adjacent.append(self[x,y-1])
+        if x + 1 < self.width:
+            adjacent.append(self[x+1, y])
+        if y + 1 < self.height:
+            adjacent.append(self[x, y+1])
+
+        return adjacent
