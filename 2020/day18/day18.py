9bb97a216b2b93719d593f3747bd0be18fb46336
Michael Haig <michael.haig@gmail.com>
2020-12-18 23:40:12 -0500

    Add Day 18 Part 1 solution

---
+++ 2020/day18/day18.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# vim:set fileencoding=utf8: #
+
+import sys
+
+def line_to_list(line):
+    result = []
+    for c in line:
+        if c == ' ':
+            next
+        else:
+            result.append(c)
+
+    return result
+
+
+def do_math(equation):
+
+    i = 0
+    answer = 0
+    while i < len(equation):
+        if equation[i] == '+':
+            i += 1
+            if equation[i] == '(':
+                i += 1
+                t = do_math(equation[i:])
+                answer += t[1]
+                i += t[0]
+            else:
+                answer += int(equation[i])
+            i += 1
+        elif equation[i] == '*':
+            i += 1
+            if equation[i] == '(':
+                i += 1
+                t = do_math(equation[i:])
+                answer *= t[1]
+                i += t[0]
+            else:
+                answer *= int(equation[i])
+            i += 1
+        elif equation[i] == '(':
+            i += 1
+            t = do_math(equation[i:])
+            answer += t[1]
+            i += t[0]
+            i += 1
+        elif equation[i].isdigit():
+            answer = int(equation[i])
+            i += 1
+        else:
+            break
+
+    return i, answer
+
+puzzle_input = sys.stdin.read().strip()
+
+answers = []
+for line in puzzle_input.split('\n'):
+
+    answers.append(do_math(line_to_list(line))[1])
+
+print('Part 1 Answer: {}'.format(sum(answers)))
