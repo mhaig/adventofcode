3c23c54d11057e17d0dca1f05ebd9b5776b5e9d0
Michael Haig <michael.haig@gmail.com>
2020-12-20 11:14:59 -0500

    Add Day 19 Part 2 work in progress

--- 2020/day19/day19.py
+++ 2020/day19/day19.py
@@ -40,7 +40,6 @@ def build_rule(rule):
     return string
 
 rule0 = build_rule('0')
-print(rule0)
 
 matches = 0
 for m in messages:
@@ -50,3 +49,46 @@ for m in messages:
             matches += 1
 
 print(f'Part 1 Answer: {matches}')
+
+# Part 2.
+
+# print('Rule 8: {}'.format(build_rule('8')))
+# print('Rule 11: {}'.format(build_rule('11')))
+# print('Rule 31: {}'.format(build_rule('31')))
+# print('Rule 42: {}'.format(build_rule('42')))
+
+rule8 = build_rule('8')
+rule42 = build_rule('42')
+print('rule8: {}'.format(rule8))
+print('rule42: {}'.format(rule42))
+
+print('Does rule8 == rule42?: {}'.format(rule8 == rule42))
+
+# First replace the rules per the puzzle:
+#   8: 42 | 42 8
+#  11: 42 31 | 42 11 31
+
+# print(rules['8'])
+# rules['8'] = '42 | 42 8'
+# print(rules['8'])
+# print(rules['11'])
+# rules['11'] = '42 31 | 42 11 31'
+# print(rules['11'])
+
+rule0 = build_rule('0')
+
+# So for rule 8, find rule 42, add an |, 42 again, and a
+#   8: 42 | 42 8  => (42 | 42 ( 42 | 42 ( 42 | 42 )))
+
+# print(rule0.find(rule42))
+# print(rule0.find(rule8))
+print(rule0)
+
+matches = 0
+for m in messages:
+    result = re.match(rule0, m)
+    if result:
+        if result.span()[0] == 0 and result.span()[1] == len(m):
+            matches += 1
+
+print(f'Part 2 Answer: {matches}')
