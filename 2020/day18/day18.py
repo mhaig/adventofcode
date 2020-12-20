59b38601d1ee8d1546fbb15772193a97b2bdabc7
Michael Haig <michael.haig@gmail.com>
2020-12-20 11:14:18 -0500

    Add Day 18 Part 2 work in progress

--- 2020/day18/day18.py
+++ 2020/day18/day18.py
@@ -61,3 +61,49 @@ for line in puzzle_input.split('\n'):
     answers.append(do_math(line_to_list(line))[1])
 
 print('Part 1 Answer: {}'.format(sum(answers)))
+
+def solve_addition(equation):
+
+    i = 0
+    while '+' in equation:
+        while i < len(equation):
+            if equation[i] == '+':
+                i += 1
+                if equation[i] == '(':
+                    i += 1
+                    t = solve_addition(equation[i:])
+                    answer += t[1]
+                    i += t[0]
+                else:
+                    answer += int(equation[i])
+                i += 1
+            # elif equation[i] == '*':
+            #     i += 1
+            #     if equation[i] == '(':
+            #         i += 1
+            #         t = do_math(equation[i:])
+            #         answer *= t[1]
+            #         i += t[0]
+            #     else:
+            #         answer *= int(equation[i])
+            #     i += 1
+            elif equation[i] == '(':
+                i += 1
+                t = do_math(equation[i:])
+                answer += t[1]
+                i += t[0]
+                i += 1
+            elif equation[i].isdigit():
+                answer = int(equation[i])
+                i += 1
+            else:
+                break
+
+        return i, answer
+
+
+
+answers = []
+for line in puzzle_input.split('\n'):
+
+    answers.append(solve_addition(line_to_list(line)))
