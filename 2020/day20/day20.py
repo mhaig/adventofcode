b515c63330eadd40c894e78089bb8a5e310d16f9
Michael Haig <michael.haig@gmail.com>
2020-12-22 11:19:27 -0500

    Add Day 20 Part 2 work in progress

--- 2020/day20/day20.py
+++ 2020/day20/day20.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # vim:set fileencoding=utf8: #
 
+import math
 import sys
 from tile import Tile
+from image import Image
 
 puzzle_input = sys.stdin.read()
 
@@ -32,13 +34,34 @@ for tile in tiles:
         if len(set(tile.get_edges()) & set(t.get_edges())):
             tile.add_shared_edge(t)
 
+    if len(tile._shared_edges) == 2:
+        corners.append(tile)
 
 # Find the corner tiles by finding the four tiles with only two shared edges.
 part1_answer = 1
-for tile in tiles:
-    if len(tile._shared_edges) == 2:
-        part1_answer *= tile._id_num
+for tile in corners:
+    part1_answer *= tile._id_num
 
 print(f'Part 1 Answer: {part1_answer}')
 
-print(len(tiles))
+# Start creating the final image.
+image = Image()
+# Take the first corner in the list and put it at 0,0 in the image.
+image[0,0] = corners[0]
+
+# Now follow the shared edges to fill in the image.
+for x in range(1,int(math.sqrt(len(tiles)))):
+    # Find the shared edge of the previous item with only 2 shared edges.
+    next_tile = next((t for t in image[x-1,0]._shared_edges if len(t._shared_edges) <= 3), None)
+    print(next_tile)
+    # print(len(list(next_tile)))
+    # quit()
+    image[x,0] = next_tile
+
+for y in range(1,int(math.sqrt(len(tiles)))):
+    next_tile = next((t for t in image[0,y-1]._shared_edges if len(t._shared_edges) <=3 and t not in image.values()), None)
+    print(next_tile)
+    image[0,y] = next_tile
+
+
+print(image)
---
+++ 2020/day20/image.py
@@ -0,0 +1,7 @@
+from aoc.grid import Grid
+
+class Image(Grid):
+
+    def __init__(self):
+        Grid.__init__(self)
+
