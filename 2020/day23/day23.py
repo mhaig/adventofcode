0b45255938b9c2e58fde78a7560546412a31d9ee
Michael Haig <michael.haig@gmail.com>
2020-12-23 14:15:06 -0500

    Add refactor of Day 23 Part 1 to support Part 2

--- 2020/day23/day23.py
+++ 2020/day23/day23.py
@@ -4,7 +4,7 @@
 import sys
 from circularlist import CircularList
 
-def get_destination(circ_list):
+def get_destination(circ_list, min_cup, max_cup):
     destination = circ_list._head.data - 1
     while destination not in circ_list:
         destination -= 1
@@ -12,6 +12,27 @@ def get_destination(circ_list):
             destination = max_cup
 
     return destination
+
+def move(move_number, cup_list, min_cup, max_cup, debug=True):
+    if debug:
+        print(f'-- move {move_number} --')
+
+    if debug:
+        print('cups: {}'.format(' '.join([str(x) for x in cup_list])))
+    pick_up = []
+    for _ in range(3):
+        pick_up.append(cup_list.remove_next())
+    if debug:
+        print('pick up: {}'.format(pick_up))
+    destination = get_destination(cup_list, min_cup, max_cup)
+    if debug:
+        print('destination: {}'.format(destination))
+    cup_list.insert_at(destination, pick_up)
+    cup_list.rotate()
+
+    if debug:
+        print()
+    return cup_list
 
 puzzle_input = sys.stdin.read().strip()
 
@@ -22,27 +43,12 @@ for x in puzzle_input:
     cups.add(int(x))
 min_cup = min(cups)
 max_cup = max(cups)
-current_cup = 0
 destination = 0
 
-while current_cup < 100:
-    print(f'-- move {current_cup+1} --')
-
-    print('cups: {}'.format(' '.join([str(x) for x in cups])))
-    pick_up = []
-    for _ in range(3):
-        pick_up.append(cups.remove_next())
-    print('pick up: {}'.format(pick_up))
-    print(cups)
-    destination = get_destination(cups)
-    print('destination: {}'.format(destination))
-    cups.insert_at(destination, pick_up)
-    print(cups)
-    cups.rotate()
-
-    current_cup += 1
-
-    print()
+move_number = 1
+while move_number <= 100:
+    cups = move(move_number, cups, min_cup, max_cup, False)
+    move_number += 1
 
 print('-- final --')
 print('cups: {}'.format(cups))
