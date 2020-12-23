b7f4de9454d13a6a22bf5285f889e362767204d6
Michael Haig <michael.haig@gmail.com>
2020-12-23 17:57:18 -0500

    Add Day 23 Part 2 solution

--- 2020/day23/circularlist.py
+++ 2020/day23/circularlist.py
@@ -11,6 +11,8 @@ class CircularList(object):
         """
         self._head = None
         self._tail = None
+        self._current = None
+        self._hash_table = {}
 
     def add(self, data):
 
@@ -25,6 +27,8 @@ class CircularList(object):
             self._tail = new_node
             self._tail.next = self._head
 
+        self._hash_table[data] = new_node
+
     def rotate(self):
         """Move 'head' to the next item in list."""
         self._head = self._head.next
@@ -32,21 +36,25 @@ class CircularList(object):
 
     def insert_at(self, target_data, data):
         # First find the target node.
-        current = self._head
-        while current.data != target_data:
-            current = current.next
+        current = self._hash_table[target_data]
 
         for x in data:
             new_node = Node(x)
             new_node.next = current.next
             current.next = new_node
             current = current.next
+            self._hash_table[x] = new_node
+
+    def get(self, target_data):
+        """Get a node that has a particular data."""
+        return self._hash_table[target_data]
 
     def remove_next(self):
         """Remove the next item in the buffer."""
         if self._head.next != self._head:
             removed_node = self._head.next
             self._head.next = removed_node.next
+            self._hash_table[removed_node.data] = None
             return removed_node.data
 
     def __str__(self):
--- 2020/day23/day23.py
+++ 2020/day23/day23.py
@@ -4,9 +4,12 @@
 import sys
 from circularlist import CircularList
 
-def get_destination(circ_list, min_cup, max_cup):
+def get_destination(circ_list, pick_list, min_cup, max_cup):
     destination = circ_list._head.data - 1
-    while destination not in circ_list:
+    if destination < min_cup:
+        destination = max_cup
+
+    while destination in pick_list:
         destination -= 1
         if destination < min_cup:
             destination = max_cup
@@ -24,7 +27,7 @@ def move(move_number, cup_list, min_cup, max_cup, debug=True):
         pick_up.append(cup_list.remove_next())
     if debug:
         print('pick up: {}'.format(pick_up))
-    destination = get_destination(cup_list, min_cup, max_cup)
+    destination = get_destination(cup_list, pick_up, min_cup, max_cup)
     if debug:
         print('destination: {}'.format(destination))
     cup_list.insert_at(destination, pick_up)
@@ -43,7 +46,6 @@ for x in puzzle_input:
     cups.add(int(x))
 min_cup = min(cups)
 max_cup = max(cups)
-destination = 0
 
 move_number = 1
 while move_number <= 100:
@@ -59,3 +61,28 @@ while cups._head.data != 1:
 
 # Now the answer is the string minus the beginning 1.
 print('Part 1 Answer: {}'.format(''.join([str(x) for x in cups if x != 1])))
+
+print('Building Part 2 data set...')
+cups = CircularList()
+for x in puzzle_input:
+    cups.add(int(x))
+min_cup = min(cups)
+max_cup = max(cups)
+for x in range(max_cup+1, 1000000+1):
+    cups.add(x)
+max_cup = max(cups)
+print('Done!')
+
+move_number = 1
+while move_number <= 10000000:
+    cups = move(move_number, cups, min_cup, max_cup, False)
+    move_number += 1
+
+    if move_number % 1000000 == 0:
+        print(f'movin... {move_number}')
+
+one = cups.get(1)
+next_after_one = one.next.data
+next_next_after_one = one.next.next.data
+print('Next two cups: {}, {}'.format(next_after_one, next_next_after_one))
+print('Part 2 Answer: {}'.format(next_after_one * next_next_after_one))
