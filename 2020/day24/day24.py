fe8e56f4642acfdf605959adb11be164fb7f11b0
Michael Haig <michael.haig@gmail.com>
2020-12-24 23:31:49 -0500

    Add Day 24 Part 2 solution

--- 2020/day24/day24.py
+++ 2020/day24/day24.py
@@ -25,6 +25,10 @@ import sys
 puzzle_input = sys.stdin.read().strip()
 
 tiles = {}
+
+def get_blacks():
+    return [k for k,v in tiles.items() if v == 'black']
+
 for line in puzzle_input.split('\n'):
     coordinates = [0, 0]
     # Follow the coordinates to find the tile that gets flipped.
@@ -62,4 +66,52 @@ for line in puzzle_input.split('\n'):
         else:
             tile[key] = 'black'
 
-print('Part 1 Answer: {}'.format((len([x for x in tiles.values() if x == 'black']))))
+print('Part 1 Answer: {}'.format((len(get_blacks()))))
+
+def get_adjacent_colors(q, r):
+    adjacent = []
+    for i,j in [[1, -1], [1, 0], [0,1], [-1, 1], [-1, 0], [0,-1]]:
+        if (q+i, r+j) in tiles:
+            adjacent.append(tiles[q+i, r+j])
+        else:
+            adjacent.append('white')
+
+    return adjacent
+
+def get_adjacent_whites(q, r):
+    adjacent = []
+    for i,j in [[1, -1], [1, 0], [0,1], [-1, 1], [-1, 0], [0,-1]]:
+        if (q+i, r+j) in tiles:
+            if tiles[q+i, r+j] == 'white':
+                adjacent.append([q+i, r+j])
+        else:
+            adjacent.append([q+i, r+j])
+    return adjacent
+
+def process_day(day):
+
+    new_tiles = dict(tiles)
+    # Get location of all the black tiles.
+    black_tiles = get_blacks()
+
+    # First process rules for the black tiles.
+    for l in black_tiles:
+        adjacent_colors = get_adjacent_colors(l[0], l[1])
+        num_blacks = len([x for x in adjacent_colors if x == 'black'])
+        if num_blacks == 0 or num_blacks > 2:
+            new_tiles[tuple(l)] = 'white'
+
+        # Get the white tiles adjacent to this black tile.
+        white_tile_locations = get_adjacent_whites(l[0], l[1])
+        for w in white_tile_locations:
+            # For each white tile, see if it has exactly two black adjacent.
+            adjacent_colors = get_adjacent_colors(w[0], w[1])
+            if len([x for x in adjacent_colors if x == 'black']) == 2:
+                new_tiles[tuple(w)] = 'black'
+
+    return new_tiles
+
+for x in range(100):
+    tiles = process_day(x)
+
+print('Part 2 Answer: {}'.format(len(get_blacks())))
