c70ce6c101e104accb41b22da894e682f7c0d414
Michael Haig <michael.haig@gmail.com>
2020-12-30 22:03:13 -0500

    Add Day 10 Part 2 work in progress

--- 2020/day10/day10.py
+++ 2020/day10/day10.py
@@ -29,3 +29,39 @@ elif rated - chain[-1] == 1:
 chain.append(rated)
 print(f'{diff1} differences of 1 jolt and {diff3} differences of 3 jolts')
 print('Part 1 Answer: {}'.format(diff1 * diff3))
+
+def build_chain(chain, sources, rated):
+    in3 = []
+    for x in sources:
+        if x - chain[-1] > 3:
+            break
+        elif x - chain[-1] > 0:
+            in3.append(x)
+
+    total = 0
+    if not in3:
+        # See if the end is 3 within our final value.
+        if (rated - chain[-1]) in [1, 2, 3]:
+            return 1
+        else:
+            return 0
+    for t in in3:
+        total += build_chain(chain + [t], sources[:sources.index(t)]+sources[sources.index(t)+1:], rated)
+
+    return total
+
+joltage_adapters = [int(x) for x in puzzle_input.split()]
+# print('Part 2 Answer: {}'.format(build_chain([0], sorted(joltage_adapters), rated)))
+
+joltage_adapters = [0] + joltage_adapters + [rated]
+joltage_adapters = sorted(joltage_adapters)
+reach = [1]
+for i,v in enumerate(joltage_adapters):
+    print(v)
+    if i:
+        reach.append(0)
+        for j in [1,2,3]:
+            if i-j >= 0 and v - joltage_adapters[i-j] <= 3:
+                reach[-1] += 1
+
+print(reach)
