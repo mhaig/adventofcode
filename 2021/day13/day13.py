8da6e31770a326384d6eab02badc6d009fa6a54a
Michael Haig <michael.haig@gmail.com>
2021-12-15 22:43:47 -0500

    Add Day 13 Part 2 solution
    This solution includes resizing the paper which makes the Part 1
    solution faster.

--- 2021/day13/day13.py
+++ 2021/day13/day13.py
@@ -23,30 +23,30 @@ class Paper(Grid):
         return string.strip()
 
     def fold_y(self, pos):
-        # print('folding y at {}'.format(pos))
         for y in range(pos+1, self.height):
             for x in range(self.width):
                 new_y = pos - y + pos
-                # print('Moving {},{} to {},{}'.format(x, y, x, new_y))
                 if (x,y) in self:
-                    self[x, new_y] = self[x,y]
-                    self[x,y] = '.'
+                    self[x, new_y] = self.pop((x,y))
 
     def fold_x(self, pos):
-        # print('folding x at {}'.format(pos))
         for x in range(pos+1, self.width):
             for y in range(self.height):
                 new_x = pos - x + pos
-                # print('Moving {},{} to {},{}'.format(x, y, new_x, y))
                 if (x,y) in self:
-                    self[new_x, y] = self[x,y]
-                    self[x,y] = '.'
+                    self[new_x, y] = self.pop((x,y))
 
     def do_fold(self, axis, pos):
         if axis == 'x':
             paper.fold_x(pos)
         else:
             paper.fold_y(pos)
+
+        self._resize()
+
+    def _resize(self):
+        self._width = max([k[0] for k in self.keys()]) + 1
+        self._height = max([k[1] for k in self.keys()]) + 1
 
 
 paper = Paper()
@@ -65,3 +65,9 @@ for line in sys.stdin.readlines():
 paper.do_fold(folds[0][0], folds[0][1])
 
 print('Part 1 Solution: {}'.format(list(paper.values()).count('#')))
+
+for f in folds[1:]:
+    paper.do_fold(f[0], f[1])
+
+print('Part 2 Solution:')
+print(paper)
