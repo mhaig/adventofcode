2826c8c1feafdf6c68e4e6e986122a0860e8b533
Michael Haig <michael.haig@gmail.com>
2022-12-02 21:15:30 -0500

    Add 2022 Day 1 solution

---
+++ 2022/day01/src/main.rs
@@ -0,0 +1,55 @@
+use std::{io::{self, BufRead}};
+
+#[derive(Debug)]
+struct Elf {
+    id: u32,
+    calories: i32,
+}
+
+fn main() -> io::Result<()> {
+    let mut lines = io::stdin().lock().lines();
+
+    let mut elfs: Vec<Elf> = Vec::new();
+
+    let mut id: u32 = 1;
+    let mut current_id: u32 = u32::MAX;
+    while let Some(line) = lines.next() {
+        let last_input = line.unwrap();
+
+        if id != current_id {
+            elfs.push(Elf {id, calories: 0});
+            current_id = id;
+        }
+
+        if last_input.len() > 0 {
+            let cals: i32 = last_input.parse().unwrap();
+
+            if let Some(e) = elfs.last_mut() {
+                e.calories += cals;
+            }
+        }
+        else {
+            id += 1;
+        }
+    }
+
+    // Sort the elves by calories.
+    elfs.sort_unstable_by(|a, b| b.calories.cmp(&a.calories));
+
+    if let Some(elf) = elfs.first() {
+        let max_calories = elf.calories;
+        let id = elf.id;
+        println!("Elf carrying the most calories: {id}, {max_calories} cals");
+    }
+    else {
+        println!("Program didn't work!");
+    }
+
+    let mut top_three_cals = 0;
+    for e in &elfs[0..3] {
+        top_three_cals += e.calories;
+    }
+    println!("Top three elfs are carying {top_three_cals} cals");
+
+    Ok(())
+}
