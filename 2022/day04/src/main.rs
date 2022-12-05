a842aab5b45e4feab42d72f541260c695ec6aa32
Michael Haig <michael.haig@gmail.com>
2022-12-04 22:42:37 -0500

    Apply `clippy` and `fmt` to Day 4

--- 2022/day04/src/main.rs
+++ 2022/day04/src/main.rs
@@ -9,28 +9,24 @@ fn main() {
         let l = line.unwrap();
 
         if !l.is_empty() {
-            let l_parts: Vec<&str> = l.split(",").collect();
+            let l_parts: Vec<&str> = l.split(',').collect();
             let elf_1 = l_parts[0];
             let elf_2 = l_parts[1];
-            let elf_1_start: u32 = elf_1.split("-").collect::<Vec<&str>>()[0].parse().unwrap();
-            let elf_1_end: u32 = elf_1.split("-").collect::<Vec<&str>>()[1].parse().unwrap();
-            let elf_2_start: u32 = elf_2.split("-").collect::<Vec<&str>>()[0].parse().unwrap();
-            let elf_2_end: u32 = elf_2.split("-").collect::<Vec<&str>>()[1].parse().unwrap();
+            let elf_1_start: u32 = elf_1.split('-').collect::<Vec<&str>>()[0].parse().unwrap();
+            let elf_1_end: u32 = elf_1.split('-').collect::<Vec<&str>>()[1].parse().unwrap();
+            let elf_2_start: u32 = elf_2.split('-').collect::<Vec<&str>>()[0].parse().unwrap();
+            let elf_2_end: u32 = elf_2.split('-').collect::<Vec<&str>>()[1].parse().unwrap();
 
-            if elf_1_start >= elf_2_start && elf_1_end <= elf_2_end {
-                fully_contains += 1;
-            } else if elf_2_start >= elf_1_start && elf_2_end <= elf_1_end {
+            if elf_1_start >= elf_2_start && elf_1_end <= elf_2_end
+                || elf_2_start >= elf_1_start && elf_2_end <= elf_1_end
+            {
                 fully_contains += 1;
             }
 
-            if elf_2_start <= elf_1_start && elf_1_start >= elf_2_start {
-                if elf_1_start <= elf_2_end {
-                    overlaps +=1;
-                }
-            } else if elf_1_start <= elf_2_start && elf_2_start >= elf_1_start {
-                if elf_2_start <= elf_1_end {
-                    overlaps += 1;
-                }
+            if elf_2_start <= elf_1_start && elf_1_start <= elf_2_end
+                || elf_1_start <= elf_2_start && elf_2_start <= elf_1_end
+            {
+                overlaps += 1;
             }
         }
     }
