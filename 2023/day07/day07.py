8d503496ac72f272f344b5cbeb0fc3aad8d5b869
Michael Haig <michael.haig@gmail.com>
2023-12-14 22:14:31 -0500

    Add 2023 Day 7 Part 2 solution

--- 2023/day07/day07.py
+++ 2023/day07/day07.py
@@ -2,12 +2,22 @@
 
 import sys
 from collections import Counter
-from itertools import groupby
+from enum import Enum
+
+
+class Score(Enum):
+    HIGH_CARD = 1
+    ONE_PAIR = 2
+    TWO_PAIR = 3
+    THREE_OF_A_KIND = 4
+    FULL_HOUSE = 5
+    FOUR_OF_A_KIND = 6
+    FIVE_OF_A_KIND = 7
 
 
 class Hand:
     def __init__(self, hand: str, bid: (int | str)):
-        self._hand = []
+        self._hand: list[int] = []
         for c in hand:
             if c == "T":
                 self._hand.append(10)
@@ -23,29 +33,58 @@ class Hand:
                 self._hand.append(int(c))
 
         self._bid = int(bid)
+        self._jokers = False
 
     def score(self) -> int:
+        score = Score.HIGH_CARD
         groups = Counter(self._hand)
+
         if len(groups) == 1:
             # Only one value in list, must be 5 of a kind
-            return 7
-        if len(groups) == 2:
+            score = Score.FIVE_OF_A_KIND
+
+        elif len(groups) == 2:
             # Only two values seen in list, could be 4 of a kind or full house.
+            score = Score.FULL_HOUSE
+
             a_count = next(iter(groups.values()))
             if a_count == 4 or a_count == 1:
-                return 6
-            return 5
-        if len(groups) == 3:
+                # Four of a kind
+                score = Score.FOUR_OF_A_KIND
+
+            # If there is a joker there is either one to make it 5 of a kind or
+            # there are 3 or 2 which still makes it 5 of a kind
+            if self._has_joker():
+                score = Score.FIVE_OF_A_KIND
+
+        elif len(groups) == 3:
             # Only three values seen in list, could be 3 of a kind or two
             # pairs.
-            for v in groups.values():
-                if v == 3:
-                    return 4
-            return 3
-        if len(groups) == 4:
-            return 2
+            score = Score.TWO_PAIR
 
-        return 1
+            if 3 in groups.values():
+                # Three of a kind.
+                score = Score.THREE_OF_A_KIND
+                if groups[1] >= 1:
+                    # There is a t least one J, promote to 4 of a kind.
+                    score = Score.FOUR_OF_A_KIND
+            else:
+                if groups[1] == 2:
+                    score = Score.FOUR_OF_A_KIND
+                elif groups[1] == 1:
+                    score = Score.FULL_HOUSE
+
+        elif len(groups) == 4:
+            score = Score.ONE_PAIR
+
+            if groups[1] >= 1:
+                # The pair is J's, promote to three of a kind.
+                score = Score.THREE_OF_A_KIND
+
+        elif self._has_joker():
+            score = Score.ONE_PAIR
+
+        return score.value
 
     def __lt__(self, other) -> bool:
         if self.score() == other.score():
@@ -59,9 +98,30 @@ class Hand:
     def __repr__(self) -> str:
         return f"Hand(hand={self._hand}, bind={self._bid})"
 
+    def _has_joker(self) -> bool:
+        if 1 in self._hand:
+            return True
+
+        return False
+
     @property
     def bid(self) -> int:
         return self._bid
+
+    @property
+    def jokers(self) -> bool:
+        return self._jokers
+
+    @jokers.setter
+    def jokers(self, value: bool) -> None:
+        self._jokers = value
+
+        for i in range(len(self._hand)):
+            if self._hand[i] == 11:
+                if value:
+                    self._hand[i] = 1
+                else:
+                    self._hand[i] == 11
 
 
 game_input = list(sys.stdin.readlines())
@@ -75,3 +135,13 @@ for i, h in enumerate(sorted(hands)):
     score += (i + 1) * h.bid
 
 print("Part 1 Solution: {}".format(score))
+
+# Reset hands for Jokers
+for h in hands:
+    h.jokers = True
+
+score = 0
+for i, h in enumerate(sorted(hands)):
+    score += (i + 1) * h.bid
+
+print("Part 2 Solution: {}".format(score))
